9a45e89c685a5228326e8ac814ae68d24e6ed302
tazbir01 <tajberahamad@gmail.com>
2023-10-23 16:02:20 +0600

    added app.post for add  product to cart db

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors())
@@ -11,8 +12,6 @@ app.use(express.json())
 // bitebliss
 // gNGL191fGSpxxlsI
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ujho7bh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,6 +28,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const biteblissCollection = client.db('biteblissDB').collection('products')
+    const brandCollection = client.db('biteblissDB').collection('brands')
+    const cartCollection = client.db('biteblissDB').collection('cart')
 
     app.post('/products', async(req, res)=>{
       const newProduct = req.body
@@ -43,9 +44,21 @@ async function run() {
       res.send(result)
     })
 
+    // cart api
+    app.post('/cart', async(req,res)=>{
+      const product = req.body
+      console.log(product)
+      const result = await cartCollection.insertOne(product)
+      res.send(result)
+    })
 
 
-
+    // brands api
+    app.get('/brands' , async(req, res)=>{
+      const cursor = brandCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
